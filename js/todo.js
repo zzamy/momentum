a535d986de76e3fc216fd8df6e111d53608ce0f4
zzamy <hatank1013@gmail.com>
2021-08-25 21:43:47 +0530

    Todo

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
+function paintToDo(newToDo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newToDo;
+  toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = "";
-  console.log(newToDo, toDoInput.value);
+  paintToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
