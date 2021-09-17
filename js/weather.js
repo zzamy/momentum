ca04b6d3c6948fa268ab2643d98091dcb1627c6c
zzamy <hatank1013@gmail.com>
2021-09-17 23:34:54 +0530

    #8.1Weather API FIN

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = "b19550c71fecc85e73bcb7b72641dc72";
+
 function onGeoOK(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log(`You live in ${lat}, ${lng}`);
+  const lon = position.coords.longitude;
+  console.log(`You live in ${lat}, ${lon}`);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url).then((response) =>
+    response.json().then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = `Country:${data.sys.country}, City:${data.name}`;
+      weather.innerText = `Temp:${data.main.temp}°C, Weather:${data.weather[0].main}`;
+    })
+  );
 }
 
 function onGeoError() {
