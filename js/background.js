c873804b5668c480b765002a93c928f8cd7aa1a1
zzamy <hatank1013@gmail.com>
2021-09-23 22:04:48 +0530

    #final_changed

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["0.jpg", "1.jpg", "2.jpg"];
+/*const images = ["0.jpg", "1.jpg", "2.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
@@ -6,4 +6,8 @@ const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+document.body.appendChild(bgImage);*/
+
+document.body.style = `background-image: url("img/${Math.ceil(
+  Math.random() * 3
+)}.jpg");`;
